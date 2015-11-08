62d99f6eb8387d5194869421b12d3e56ded8ec57
gfhf <re.goetsch@googlemail.com>
2015-11-08 14:05:17 +0100

    Praktikum 2

--- cg_exercises/src/main/java/computergraphics/scenegraph/TriangleMeshNode.java
+++ cg_exercises/src/main/java/computergraphics/scenegraph/TriangleMeshNode.java
@@ -12,16 +12,31 @@ import computergraphics.math.Vector3;
 public class TriangleMeshNode extends Node
 {
 	private HalfEdgeTriangleMesh triangleMesh;
+	private int displayList;
 	
 	public TriangleMeshNode(HalfEdgeTriangleMesh mesh){
 		this.triangleMesh = mesh;
+		this.displayList = 0;
 	}
 
+	
 	@Override
 	public void drawGl(GL2 gl) {
-        // Remember current state of the render system
-        
-        Vector3[] vertexPositions = triangleMesh.getAllVertexPositions();
+        if(displayList == 0){
+        	System.out.println("Test");
+        	buildList(gl);
+        }
+        else{
+        	System.out.println("test2");
+        	gl.glCallList(displayList);
+        }
+    }	
+	
+	public void buildList(GL2 gl){
+		this.displayList = gl.glGenLists(1);
+		gl.glNewList(displayList, GL2.GL_COMPILE);
+		gl.glBegin(GL2.GL_TRIANGLES);
+		Vector3[] vertexPositions = triangleMesh.getAllVertexPositions();
 
         List<TriangleFacet> triangles = triangleMesh.getTriangles();
         
@@ -37,13 +52,14 @@ public class TriangleMeshNode extends Node
             Vector3 v2Position = v2.getPosition();
             Vector3 v3Position = v3.getPosition();
             		
-            gl.glBegin(GL2.GL_TRIANGLES);
+            
             gl.glNormal3d(normal.getX(), normal.getY(), normal.getZ());
             gl.glVertex3d(v1Position.getX(), v1Position.getY(), v1Position.getZ());
             gl.glVertex3d(v2Position.getX(), v2Position.getY(), v2Position.getZ());
             gl.glVertex3d(v3Position.getX(), v3Position.getY(), v3Position.getZ());
-            gl.glEnd();
+            
         }
-    }
-
+        gl.glEnd();
+        gl.glEndList();
+	}
 }
